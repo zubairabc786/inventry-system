805280818ebdb275344523d3816a057c7367d1ef
Muhammad Zubair <mzubairabc947@gmail.com>
2025-07-16 13:46:33 +0500

    seven commit

--- app/(inventory)/components/DashboardCard.jsx
+++ app/(inventory)/components/DashboardCard.jsx
@@ -1,11 +1,11 @@
-export default function DashboardCard({ title, value, icon }) {
-  return (
-    <div className="bg-white p-4 rounded-2xl shadow-md flex items-center space-x-4">
-      <div className="text-3xl">{icon}</div>
-      <div>
-        <div className="text-sm text-gray-500">{title}</div>
-        <div className="text-xl font-semibold">{value}</div>
-      </div>
-    </div>
-  );
-}
+// export default function DashboardCard({ title, value, icon }) {
+//   return (
+//     <div className="bg-white p-4 rounded-2xl shadow-md flex items-center space-x-4">
+//       <div className="text-3xl">{icon}</div>
+//       <div>
+//         <div className="text-sm text-gray-500">{title}</div>
+//         <div className="text-xl font-semibold">{value}</div>
+//       </div>
+//     </div>
+//   );
+// }
