022042f36c5ca3432a641ffb37f275b0d793172f
Muhammad Zubair <mzubairabc947@gmail.com>
2025-07-16 13:24:09 +0500

    six commit

--- app/action/action.jsx
+++ app/action/action.jsx
@@ -800,20 +800,6 @@ export async function getJournalEntries1(
 }
 
 ////////////// DashBoard Total Sale,Products,Purchases
-// export async function getDashboardData() {
-//   const sale = await prisma.Purchase.count({
-//     where: {
-//       doc_type: "SV",
-//     },
-//   });
-//   const purchase = await prisma.Purchase.count({
-//     where: {
-//       doc_type: "PV",
-//     },
-//   });
-//   const product = await prisma.Product.count();
-//   return { sale, purchase, product };
-// }
 
 export async function getDashboardData() {
   try {
@@ -830,59 +816,6 @@ export async function getDashboardData() {
 }
 ////////////// Get Stock Less than 5 items
 
-// export async function getStockTableLessFive() {
-//   const purchases = await prisma.Purchase.groupBy({
-//     by: ["product_code"],
-//     where: {
-//       doc_type: "PV",
-//     },
-//     _sum: {
-//       quantity: true,
-//     },
-//   });
-
-//   const sales = await prisma.Purchase.groupBy({
-//     by: ["product_code"],
-//     where: {
-//       doc_type: "SV",
-//     },
-//     _sum: {
-//       quantity: true,
-//     },
-//   });
-
-//   const salesMap = {};
-//   for (const s of sales) {
-//     salesMap[s.product_code] = s._sum.quantity || 0;
-//   }
-
-//   const stockData = [];
-
-//   for (const p of purchases) {
-//     const product = await prisma.Product.findUnique({
-//       where: { product_code: p.product_code },
-//     });
-
-//     const purchaseQty = p._sum.quantity || 0;
-//     const saleQty = salesMap[p.product_code] || 0;
-//     const stockQty = purchaseQty - saleQty;
-
-//     // console.log("StockQty=", stockQty);
-//     // Only include if stock is less than 5
-//     if (stockQty < 5) {
-//       stockData.push({
-//         item_code: p.product_code,
-//         item_name: product?.product_name || "",
-//         // Purchase: purchaseQty,
-//         // Sale: saleQty,
-//         Stock: stockQty,
-//         doc_type: "Stock",
-//       });
-//     }
-//   }
-
-//   return stockData;
-// }
 export async function getStockTableLessFive() {
   try {
     const [purchases, sales] = await Promise.all([
