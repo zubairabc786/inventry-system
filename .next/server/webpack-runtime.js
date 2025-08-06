ccefd5804671a5a86aa0c63d738dabd110d92c0f
Muhammad Zubair <mzubairabc947@gmail.com>
2025-08-06 20:32:56 +0500

    twenty one commit

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -30,7 +30,7 @@
 /******/ 		// Execute the module function
 /******/ 		var threw = true;
 /******/ 		try {
-/******/ 			__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 			__webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__);
 /******/ 			threw = false;
 /******/ 		} finally {
 /******/ 			if(threw) delete __webpack_module_cache__[moduleId];
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("280bf7719ed0e25b")
+/******/ 		__webpack_require__.h = () => ("852ec29f1e523fda")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- app/(inventory)/sale-detail/page.jsx
+++ app/(inventory)/sale-detail/page.jsx
@@ -1,5 +1,4 @@
 import { getInventWithSale } from "../../action/action";
-import DeletePurchase from "../components/DeletePurchase";
 import SearchableSaleList from "../components/SearchableSaleList";
 import Link from "next/link";
 export default async function InventPage() {
@@ -13,68 +12,6 @@ export default async function InventPage() {
         </span>
       </h1>
       <SearchableSaleList invents={invents} />
-      {/* {invents.map((invent) => (
-        <div key={invent.id} className="border p-4 mb-4 rounded-lg shadow">
-          <div className="flex justify-between">
-            <div>
-              <p>
-                <strong>Invoice No:</strong> {invent.invoice_no}
-              </p>
-              <p>
-                <strong>Doc ID:-</strong> {invent.doc_id}
-              </p>
-              <p>
-                <strong>Doc Type:-</strong> {invent.doc_type}
-              </p>
-              <p>
-                <strong>Account:</strong> {invent.COA?.account_name}
-              </p>
-              <p>
-                <strong>Date:</strong>{" "}
-                {new Date(invent.dated).toLocaleDateString()}
-              </p>
-            </div>
-            <div className="flex gap-3 h-12">
-              <Link
-                href={`/sale-edit/${invent.doc_id}`}
-                className="p-2 bg-blue-700 text-white rounded"
-              >
-                Update Sale
-              </Link>
-              <DeletePurchase Sale={"Sale"} doc_id={invent.doc_id} />
-            </div>
-          </div>
-          <div className="mt-2">
-            <h2 className="font-bold text-xl text-blue-600 underline">
-              Sales:-
-            </h2>
-            <div className="grid grid-cols-5 text-green-500">
-              <label className="font-bold">Product Name</label>
-              <label className="font-bold">Quantity</label>
-              <label className="font-bold">Price</label>
-              <label className="font-bold">Remarks</label>
-              <label className="font-bold">Amount</label>
-            </div>
-            {invent.Purchase.map((p) => (
-              <div key={p.id} className=" ml-1 grid grid-cols-5 border-l  mt-2">
-                <p>
-                  {p.Product?.product_name} ({p.product_code})
-                </p>
-
-                <p>{p.quantity}</p>
-                <p>{p.price}</p>
-                <p>{p.remarks}</p>
-                <p className="">{p.amount}</p>
-              </div>
-            ))}
-            <div className="flex justify-end mr-12">
-              <p className="font-bold mt-2">
-                G-Total:{invent.Purchase[0].grandTotal}
-              </p>
-            </div>
-          </div>
-        </div>
-      ))} */}
       <div className="flex justify-center mt-4 bg-blue-900">
         <Link href="/sale-sheet" className="text-white p-2">
           Go To Sale Sheet
